6f3da35cadd8ada0b274ae5ce3f244eef301a4c5
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-11 07:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+	let word = str.trim();
+	let arr = word.split(" ");
+	let w = arr.join("");
 
+	let i = 0, j = w.length-1;
+	while(i < j){
+		if(w.charAt(i) != w.charAt(j)){
+			console.log(false);
+			return false;
+		}
+		i++;
+		j--;
+	}
+	console.log(true);
+	return true;
 }
 module.exports = palindrome
